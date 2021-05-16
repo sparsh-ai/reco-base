95a9f5e6c85c9173914469a254f63703e9a2d81c
Sparsh Agarwal <sparsh.cloud@gmail.com>
2021-05-16 13:22:31 +0530

    added docs

---
+++ .history/docs/conf_20210516120923.py
@@ -0,0 +1,24 @@
+project = 'recobase'
+copyright = '2021, Sparsh A.'
+author = 'Sparsh A.'
+
+extensions = ['sphinx.ext.autodoc',
+              'nbsphinx',
+              'sphinx.ext.mathjax',
+              'sphinx.ext.githubpages',
+              'IPython.sphinxext.ipython_console_highlighting',
+              'rst2pdf.pdfbuilder'
+]
+
+source_suffix = ['.rst', '.ipynb']
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
+master_doc = 'index'
+
+import os
+import sys
+sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('..'))
+sys.path.insert(0, os.path.abspath('../code'))
